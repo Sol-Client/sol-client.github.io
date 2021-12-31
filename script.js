6ca1f413efaed1faa1b1232211fc6d30c5a83668
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-12-31 15:40:34 +0000

    Auto download link

---
+++ script.js
@@ -0,0 +1,41 @@
+var downloadButton = document.getElementById("download-button");
+
+
+// https://stackoverflow.com/a/38241481/7658797
+function getOSExtension() {
+	var userAgent = window.navigator.userAgent;
+	var platform = window.navigator.platform;
+	var macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K", "Darwin"];
+	var windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
+
+	if(macosPlatforms.indexOf(platform) !== -1) {
+		return ".dmg";
+	}
+	else if(windowsPlatforms.indexOf(platform) !== -1) {
+		return ".exe";
+	}
+	else if(/Linux/.test(platform)) {
+		return ".deb";
+	}
+}
+
+var os = getOSExtension();
+
+if(os) {
+	var request = new XMLHttpRequest();
+	request.addEventListener("load", (event) => {
+		var response = JSON.parse(request.responseText);
+		for(var asset of response.assets) {
+			if(asset.name.endsWith(os)) {
+				downloadButton.href = asset.browser_download_url;
+				downloadButton.innerText = "Download";
+				return;
+			}
+		}
+	});
+	request.open("GET", "https://api.github.com/repos/TheKodeToad/Sol-Client/releases/latest");
+	request.send();
+}
+else {
+	downloadButton.innerText = "Unsupported";
+}
